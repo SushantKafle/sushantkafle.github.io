de0ca938b53f2363f0cb7b16c9a4cf19cf9a0276
Sushant Kafle <sushantkafle@LATLabs-MacBook-Pro.local>
2019-01-22 10:14:02 -0500

    website redecoration; font-changes; layout changes etc.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
 
+(function($) {          
+    $(document).ready(function(){ 
+    	$('#header').hide();    	                   
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 500) {
+                $('#header').show(200);
+            } else {
+                $('#header').fadeOut(200);
+            }
+        });
+    });
+})(jQuery);
